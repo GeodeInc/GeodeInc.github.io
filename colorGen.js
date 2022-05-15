d02167e7aaff042d3cb50d02d02ae5dea0875f46
GeodeInc <99336019+GeodeInc@users.noreply.github.com>
2022-05-15 12:06:39 -0400

    Update colorGen.js

--- colorGen.js
+++ colorGen.js
@@ -37,7 +37,7 @@ function load (){
       hexDiv[index].innerText = item
       hexVal +=item + '-'
     })
-    window.history.replaceState(null, document.title, 'https://COOLORS.taylorhoughtal1.repl.co/' + hexVal)
+     window.history.replaceState(null, document.title, 'geodeinc.me/' + hexVal)
     hexVal=''
 }
 function copyAndPaste(element){
