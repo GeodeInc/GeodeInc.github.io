1c70e60a650a43300c46cd3bc05b816ad3f250a2
GeodeInc <99336019+GeodeInc@users.noreply.github.com>
2022-05-15 12:20:10 -0400

    Update colorGen.js

--- colorGen.js
+++ colorGen.js
@@ -13,7 +13,7 @@ document.addEventListener('keydown', event => {
       hexDiv[index].innerText = item
       hexVal += item + '-'
     })
-    window.history.replaceState( hexVal)
+    window.history.replaceState(null, hexVal)
     hexVal=''
   }
 })
@@ -37,7 +37,7 @@ function load (){
       hexDiv[index].innerText = item
       hexVal +=item + '-'
     })
-     window.history.replaceState( hexVal)
+     window.history.replaceState(null, hexVal)
     hexVal=''
 }
 function copyAndPaste(element){
