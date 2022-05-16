48f1b88d89b26cba2be93665b6fe1609908a1d98
Sean The Bean <sdevine815@gmail.com>
2022-05-16 08:02:23 -0400

    Update colorGen.js

--- colorGen.js
+++ colorGen.js
@@ -13,7 +13,7 @@ document.addEventListener('keydown', event => {
       hexDiv[index].innerText = item
       hexVal += item + '-'
     })
-    window.history.replaceState(null, document.title, 'https://geodeinc.me' + hexVal)
+    window.location.replace('https://geodeinc.me' + hexVal)
     hexVal=''
   }
 })
