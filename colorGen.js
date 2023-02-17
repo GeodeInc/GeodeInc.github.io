4cfd974e4ac0ed4b77e1ed564acf4e9f74f9afe4
GeodeInc <99336019+GeodeInc@users.noreply.github.com>
2023-02-17 10:42:51 -0500

    Update colorGen.js

--- colorGen.js
+++ colorGen.js
@@ -97,7 +97,7 @@ var rgbToHex = (r, g, b) => {
 
 //converts the given hex to rgb
 var hexToRgb = (hex) => {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  var result = /^?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result ? { r: parseInt(result[1], 16), g: parseInt(result[2], 16), b: parseInt(result[3], 16) } : null;
 }
 
